0038e74821f1ef97af6c0cce022ce74aa7f12674
carlos060798 <daniloangaritagarcia@gmail.com>
2023-12-29 17:57:38 -0500

    version 11.2.2

--- apprestaurante/src/compont/admin/componet/ReservationForm.tsx
+++ apprestaurante/src/compont/admin/componet/ReservationForm.tsx
@@ -1,39 +1,41 @@
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Col } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalendarAlt, faClipboardList, faUsers, faPlus } from '@fortawesome/free-solid-svg-icons';
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faCalendarAlt,
-  faClipboardList,
-  faUsers,
-} from "@fortawesome/free-solid-svg-icons";
-function ReservationForm() {
+const ReservationForm = () => {
   return (
-    <Form>
+    <Col xs={12} md={10} lg={8} >
+    <Form className="border p-4 rounded">
+      <h2 className="text-center mb-4">
+        <FontAwesomeIcon icon={faCalendarAlt} className="mr-2" />
+        Reserva de Mesa
+      </h2>
+
       <Form.Group controlId="fechaReserva">
         <Form.Label>
-          <FontAwesomeIcon icon={faCalendarAlt} className="mr-2" /> Fecha de
-          Reserva
+          <FontAwesomeIcon icon={faCalendarAlt} className="mr-2" />
+          Fecha de Reserva
         </Form.Label>
         <Form.Control type="date" name="fechaReserva" />
       </Form.Group>
 
       <Form.Group controlId="tipoReserva">
         <Form.Label>
-          <FontAwesomeIcon icon={faClipboardList} className="mr-2" /> Tipo de
-          Reserva
+          <FontAwesomeIcon icon={faClipboardList} className="mr-2" />
+          Tipo de Reserva
         </Form.Label>
         <Form.Control
           type="text"
           name="tipoReserva"
-          placeholder="Ingrese el tipo de reserva"
+          placeholder="Ej. Cumpleaños, Aniversario, etc."
           required
         />
       </Form.Group>
 
       <Form.Group controlId="cantidadPersonas">
         <Form.Label>
-          <FontAwesomeIcon icon={faUsers} className="mr-2" /> Cantidad de
-          Personas
+          <FontAwesomeIcon icon={faUsers} className="mr-2" />
+          Cantidad de Personas
         </Form.Label>
         <Form.Control
           type="number"
@@ -44,9 +46,11 @@ function ReservationForm() {
       </Form.Group>
 
       <Button variant="primary" type="submit" className="w-100 mt-3">
+        <FontAwesomeIcon icon={faPlus} className="mr-2" />
         Crear Reserva
       </Button>
     </Form>
+    </Col>
   );
 }
 
--- apprestaurante/src/compont/admin/componet/createreservation.tsx
+++ apprestaurante/src/compont/admin/componet/createreservation.tsx
@@ -1,17 +1,33 @@
-import {Container, Row, Col } from "react-bootstrap";
-import ReservationForm from "./ReservationForm";
 
-function CreateRerseva() {
+import { Container, Col} from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronCircleRight } from '@fortawesome/free-solid-svg-icons';
+import ReservationForm from './ReservationForm';
+
+const ReservaView = () => {
   return (
-    <Container className="d-flex align-items-center justify-content-center vh-100">
-      <Row className="w-100">
-        <Col md={8} lg={6} xl={4} className="mx-auto">
-          <h2 className="text-center mb-4">Crear Reserva</h2>
-          <ReservationForm/>
+    <Container className="d-flex my-5">
+      
+        <Col md={6} xs={12} className="align-self-center">
+          <div className="text-center">
+            <h2 className="mb-4">¡Reserva tu mesa!</h2>
+            <p>
+              ¿Listo para disfrutar de una experiencia única? ¡Haz tu reserva ahora!
+            </p>
+            <div className='bg-primary  my-5 rounded-2 text-light'>
+              <FontAwesomeIcon icon={faChevronCircleRight} className="mr-2" />
+              Reservar Mesa
+            </div>
+          </div>
         </Col>
-      </Row>
+
+        {/* Columna del formulario */}
+        <Col md={6} xs={12} className="d-flex align-items-center justify-content-center">
+          <ReservationForm />
+        </Col>
+      
     </Container>
   );
-}
+};
 
-export default CreateRerseva;
+export default ReservaView;
